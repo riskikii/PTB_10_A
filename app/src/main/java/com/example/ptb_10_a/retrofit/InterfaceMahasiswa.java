2aff16d152b12a1d5f9264bfca8f49a796806159
Rizki Juni Darmawan <90382451+riskikii@users.noreply.github.com>
2022-12-28 00:03:10 +0700

    ALHAMDULILLAH FINISH

--- app/src/main/java/com/example/ptb_10_a/retrofit/InterfaceMahasiswa.java
+++ app/src/main/java/com/example/ptb_10_a/retrofit/InterfaceMahasiswa.java
@@ -3,6 +3,8 @@ package com.example.ptb_10_a.retrofit;
 import com.example.ptb_10_a.models.LoginResponse;
 import com.example.ptb_10_a.models.LogoutResponse;
 import com.example.ptb_10_a.models.ProfileResponse;
+import com.example.ptb_10_a.models.UbahPassword;
+import com.example.ptb_10_a.models.UpdateProfilResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
@@ -22,5 +24,13 @@ public interface InterfaceMahasiswa {
 
     @POST("api/logout/")
     Call<LogoutResponse> logout(@Header("token")String token);
+
+    @FormUrlEncoded
+    @POST("/api/password")
+    Call<UbahPassword> UbahPassword(@Header("Authorization") String token, @Field("old_password") String old_password, @Field("new_password") String new_password, @Field("confirm_password") String confirm_password);
+
+    @FormUrlEncoded
+    @POST("/api/me/update")
+    Call<UpdateProfilResponse> updateProfile(@Header("Authorization") String token, @Field("email") String email, @Field("name") String name);
 
 }
