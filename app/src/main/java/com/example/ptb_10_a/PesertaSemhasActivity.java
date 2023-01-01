31b0a2220f5ae2022f9337aaceb1d97462a15d31
Taufik Hidayat <113841412+taufikhdytt1001@users.noreply.github.com>
2023-01-01 17:28:33 +0700

    Update PesertaSemhasActivity.java

--- app/src/main/java/com/example/ptb_10_a/PesertaSemhasActivity.java
+++ app/src/main/java/com/example/ptb_10_a/PesertaSemhasActivity.java
@@ -11,7 +11,7 @@ public class PesertaSemhasActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addpeserta);
+        setContentView(R.layout.activity_addpesertasemhas);
     }
 
     public void TambahPeserta(View view) {
